92a2fc0635bd6632654585f443e11afa984200a6
mywns123 <wnsduq2000@naver.com>
2021-05-07 17:05:24 +0900

    2

--- src/main/java/spring5_aop_study/MainAspect.java
+++ src/main/java/spring5_aop_study/MainAspect.java
@@ -9,10 +9,16 @@ public class MainAspect {
 
 	public static void main(String[] args) {
 		try(AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCix.class)){
-			Calculator cal = ctx.getBean("calculator1",Calculator.class);
-			long fiveFact = cal.factorial(5);
-			System.out.printf("cal.factorial(5) = %d%n",fiveFact );
-			System.out.println(cal.getClass().getName());
+			Calculator cal1 = ctx.getBean("calculator1",Calculator.class);
+			long fiveFact1 = cal1.factorial(5);
+			System.out.printf("cal1.factorial(5) = %d%n",fiveFact1 );
+			System.out.println(cal1.getClass().getName());
+			
+			Calculator cal2 = ctx.getBean("calculator2",Calculator.class);
+			long fiveFact2 = cal2.factorial(5);
+			System.out.printf("cal2.factorial(5) = %d%n",fiveFact2 );
+			System.out.println(cal2.getClass().getName());
+			
 		}
 
 	}
